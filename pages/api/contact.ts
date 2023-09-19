143ebc709c32c9df9ab4d42552cfbf2e87b86559
Laniran JohnPaul <jplaniran01@gmail.com>
2023-09-19 18:34:15 +0100

    api

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,11 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   const data = req.body;
   console.log(data);
-  // if (req.method === "POST") {
-  //   const data = JSON.parse(req.body);
-  //   console.log(data);
-  // }
+
   return res.status(200).json({ message: data });
-  // return res.status(400).json({ message: "Bad request. Please try again" });
 };
 export default handler;
